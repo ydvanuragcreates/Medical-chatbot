ef60519873c16d428d9526d38156e6c6e2eafd60
ydvanurag <ydvanurag2022@gmail.com>
2025-06-22 02:45:56 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'Generative AI project',
+    version = '0.0.0',
+    author = 'Anurag Yadav',
+    author_email = 'anuragyadav.creates@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
